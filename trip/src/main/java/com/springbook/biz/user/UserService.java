b9d09357df9f2d1813352a55a95ed8bb695afa91
DESKTOP-6K4HTBG\PC-15 <tjrdud471@naver.com>
2022-10-28 10:31:34 +0900

    myinfo_1028_v1

---
+++ trip/src/main/java/com/springbook/biz/user/UserService.java
@@ -0,0 +1,17 @@
+package com.springbook.biz.user;
+
+public interface UserService {
+	int insert(UserVO vo);
+
+	int idCheck(UserVO vo);
+
+	UserVO login(UserVO vo);
+	UserVO find(UserVO vo);
+	int change(UserVO vo);
+	
+	UserVO info(UserVO vo);
+	
+	void update(UserVO vo);
+	
+	void delete(UserVO vo);
+}
